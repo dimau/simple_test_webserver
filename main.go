640b65ea2948f0a1e5cde316993c7de5c46a5464
Dmitrii Ushakov <dimau777@gmail.com>
2022-02-07 22:34:47 +0300

    Persisting data in the DataBase PostgreSQL
    - add select example
    - add create table handler
    - add insert handler

--- main.go
+++ main.go
@@ -1,207 +1,157 @@
 package main
 
 import (
-	"github.com/google/uuid"
-	"golang.org/x/crypto/bcrypt"
+	"database/sql"
+	"fmt"
+	_ "github.com/jackc/pgx/v4/stdlib"
 	"io"
 	"log"
 	"net/http"
-	"text/template"
-	"time"
+	"strconv"
 )
 
-var tpl *template.Template // инициализация контейнера с шаблонами HTML страниц
-var userDB map[string]user
-var sessionDB map[string]session
-
-type user struct {
-	FirstName string
-	LastName string
-	Username string
-	Password []byte
-}
-
-type session struct {
-	Username string
-	CreationTime time.Time
-}
-
-func init() {
-	tpl = template.Must(template.ParseGlob("templates/*.gohtml"))
-	userDB = map[string]user{}
-	sessionDB = map[string]session{}
-}
+// Соединение с БД объявляем доступным в рамках всего пакета
+var db *sql.DB
+var err error
 
 func main() {
+	// С помощью "sql.Open" мы инициализируем пул для хранения соединений с базой данных "db" с типом "*sql.DB"
+	// urlExample := "postgres://username:password@localhost:5432/database_name"
+	db, err = sql.Open("pgx", "postgres://evoniser_user:fdjf34HFn345Uj5n@localhost:5432/evoniser_db")
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+	// It is idiomatic to defer db.Close() if the sql.DB should not have a lifetime beyond the scope of the function
+	defer db.Close()
+
+	// Проверяем, что БД нам отвечает
+	// The first actual connection to the underlying datastore will be established lazily, when it’s needed for the first time.
+	// If you want to check right away that the database is available and accessible (for example, check that you
+	// can establish a network connection and log in), use db.Ping() to do that
+	if err = db.Ping(); err != nil {
+		log.Fatalln(err.Error())
+	}
+
 	http.HandleFunc("/", indexPageHandler)
-	http.HandleFunc("/signup", signUpPageHandler)
-	http.HandleFunc("/login", logInPageHandler)
-	http.HandleFunc("/logout", logoutPageHandler)
-	http.HandleFunc("/secretpage", onlyForRegisteredUsersPageHandler)
-	http.HandleFunc("/favicon.ico", notFoundPageHandler)
+	http.HandleFunc("/select", selectPageHandler)
+	http.HandleFunc("/createtable", createTablePageHandler)
+	http.HandleFunc("/insert", insertPageHandler)
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatalln(err.Error())
 	}
 }
 
-func indexPageHandler(w http.ResponseWriter, req *http.Request) {
-	user := getUser(w, req)
+func selectPageHandler(w http.ResponseWriter, r *http.Request) {
 
-	// executing template for HTML page
-	if err := tpl.ExecuteTemplate(w, "index.gohtml", user); err != nil {
-		log.Fatalln(err.Error())
-	}
-}
-
-func logInPageHandler(w http.ResponseWriter, r *http.Request) {
-	// Отправляем на главную страницу, если пользователь уже авторизован
-	if isLoggedIn(r) {
-		http.Redirect(w, r, "/", http.StatusSeeOther)
+	// Выполнение SQL запроса к БД, результат записываем в "rows"
+	rows, err := db.Query(`SELECT task_id, summary FROM tasks`)
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
 
-	// Обработка данных из заполненной формы
-	if r.Method == http.MethodPost {
+	// Важно выполнять Close для значений с типом *sql.Rows после завершения работы,
+	// чтобы освобождать соответствующее соединение с базой данных, иначе может быть перерасход памяти
+	defer rows.Close()
 
-		// Получаем поля из формы
-		if err := r.ParseForm(); err != nil {
-			log.Fatalln(err.Error())
-		}
-		username := r.FormValue("email")
-		password := []byte(r.FormValue("password"))
+	// Объявляем переменные, в которые будем считывать значения столбцов из строк, хранящихся в rows
+	var (
+		task_id, summary string
+		oneResult []string  // Будем упаковывать каждую пару task_id и summary в такой массив
+		allResults [][]string // Будем упаковывать в итоге все результаты выполнения запроса в одну переменную
+	)
 
-		// Находим пользователя с таким именем в базе и проверяем пароль
-		u, ok := userDB[username]
-		if !ok {
-			http.Error(w, "Username does not exist", http.StatusNotFound)
-			return
-		}
-		if err:= bcrypt.CompareHashAndPassword(u.Password, password); err != nil {
-			http.Error(w, "Password is not correct", http.StatusForbidden)
-			return
-		}
-
-		// Создаем сессию
-		sID := uuid.New().String()
-		c := &http.Cookie{
-			Name:  "session",
-			Value: sID,
-			MaxAge: int(sessionLengthInHours.Seconds()),
-		}
-		sessionDB[sID] = session{
-			Username: username,
-			CreationTime: time.Now(),
-		}
-		http.SetCookie(w, c)
-
-		// Редирект на главную после успешной регистрации
-		http.Redirect(w, r, "/", http.StatusSeeOther)
-		return
-	}
-
-	// Выполнение шаблона и отправка страницы с формой
-	if err := tpl.ExecuteTemplate(w, "login.gohtml", nil); err != nil {
-		log.Fatalln(err.Error())
-	}
-}
-
-func logoutPageHandler(w http.ResponseWriter, r *http.Request) {
-	if !isLoggedIn(r) {
-		http.Redirect(w, r, "/", http.StatusSeeOther)
-		return
-	}
-
-	// Удаляем информацию о сессии из БД на сервере
-	c, _ := r.Cookie("session")
-	delete(sessionDB, c.Value)
-
-	// Очищаем куку с id сессии на стороне клиента
-	c = &http.Cookie{
-		Name: "session",
-		Value: "",
-		MaxAge: -1,
-	}
-	http.SetCookie(w, c)
-
-	// Редирект на главную страницу сайта
-	http.Redirect(w, r, "/", http.StatusSeeOther)
-}
-
-func signUpPageHandler(w http.ResponseWriter, r *http.Request) {
-	// Отправляем на главную страницу, если пользователь уже авторизован
-	if isLoggedIn(r) {
-		http.Redirect(w, r, "/", http.StatusSeeOther)
-		return
-	}
-
-	// Обработка данных из заполненной формы
-	if r.Method == http.MethodPost {
-
-		// Получаем поля из формы
-		if err := r.ParseForm(); err != nil {
-			log.Fatalln(err.Error())
-		}
-		user := user{
-			FirstName: r.FormValue("first_name"),
-			LastName:  r.FormValue("last_name"),
-			Username:  r.FormValue("email"),
-			Password:  []byte(r.FormValue("password")),
-		}
-
-		// Проверяем есть ли такой пользователь в базе
-		if _, ok := userDB[user.Username]; ok {
-			http.Error(w, "This username is already exist", http.StatusForbidden)
-			return
-		}
-
-		// Шифруем пароль и сохраняем нового пользователя в базу
-		encryptedPass, err := bcrypt.GenerateFromPassword(user.Password, bcrypt.DefaultCost)
+	// Итерируем по строкам результата с помощью "rows.Next()"
+	for rows.Next() {
+		// We read the columns in each row into variables with rows.Scan()
+		err = rows.Scan(&task_id, &summary)
 		if err != nil {
 			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 			return
 		}
-		user.Password = encryptedPass
-		userDB[user.Username] = user
-
-		// Создаем сессию
-		sID := uuid.New().String()
-		c := &http.Cookie{
-			Name: "session",
-			Value: sID,
-			MaxAge: int(sessionLengthInHours.Seconds()),
-		}
-		sessionDB[sID] = session{
-			Username:     user.Username,
-			CreationTime: time.Now(),
-		}
-		http.SetCookie(w, c)
-
-		// Редирект на главную после успешной регистрации
-		http.Redirect(w, r, "/", http.StatusSeeOther)
-		return
+		// Собираем результаты в один массив для дальнейшей передачи в HTML шаблон для отображения
+		oneResult = []string{task_id, summary}
+		allResults = append(allResults, oneResult)
 	}
 
-	// Выполнение шаблона и отправка страницы с формой
-	if err := tpl.ExecuteTemplate(w, "signup.gohtml", nil); err != nil {
-		log.Fatalln(err.Error())
-	}
-
-}
-
-func onlyForRegisteredUsersPageHandler(w http.ResponseWriter, r *http.Request) {
-	if !isLoggedIn(r) {
-		http.Redirect(w, r, "/", http.StatusSeeOther)
-		return
-	}
-	if _, err := io.WriteString(w, "<!DOCTYPE html><html><head><title>Secret Page</title></head><body>This is a secret page</body></html>"); err != nil {
-		log.Fatalln(err.Error())
-	}
-}
-
-func notFoundPageHandler(w http.ResponseWriter, _ *http.Request) {
-	w.WriteHeader(404)
-	_, err := io.WriteString(w, "<!DOCTYPE html><html><head><title>Not Found Page</title></head><body><p>This page is not found</p></body></html>")
+	// Выход из цикла мог случиться не потому что обработаны все строки, а потому что при итерировании по ним произошла ошибка
+	// Поэтому всегда следует проверять наличие ошибки после выхода из цикла "for rows.Next()"
+	err = rows.Err()
 	if err != nil {
-		log.Fatalln(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
 	}
+
+	// Вырожденный способ собрать строку и отправить ее в качестве ответа в браузер
+	_, _ = io.WriteString(w, gatherAllResultsIntoString(allResults))
+}
+
+func gatherAllResultsIntoString(allResults [][]string) string {
+	allResultsString := ""
+	for _, i := range allResults {
+		for _, j := range i {
+			allResultsString += j + " "
+		}
+		allResultsString += "\n"
+	}
+	return allResultsString
+}
+
+func createTablePageHandler(w http.ResponseWriter, r *http.Request) {
+
+	// Создаем шаблон для выполнения (особенно повторяющихся) операций в БД - statement
+	stmt, err := db.Prepare(`CREATE TABLE customer (name VARCHAR(20));`)
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	// Выполняем запрос из шаблона stmt
+	result, err := stmt.Exec()
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	// Возвращает количество строк, которые были изменены после выполнения SQL запроса
+	n, err := result.RowsAffected()
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	// Вырожденный способ собрать строку и отправить ее в качестве ответа в браузер
+	_, _ = io.WriteString(w, "Table is successfully created: " + strconv.FormatInt(n, 10))
+}
+
+func insertPageHandler(w http.ResponseWriter, r *http.Request) {
+
+	// Создаем шаблон для выполнения (особенно повторяющихся) операций в БД - statement
+	stmt, err := db.Prepare("INSERT INTO customer (name, surname) VALUES ($1, $2);")
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	// Выполняем запрос из шаблона stmt с подстановкой переменных
+	res, err := stmt.Exec("Richard", "Stevenson")
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	// Возвращает количество строк, которые были изменены после выполнения SQL запроса
+	n, err := res.RowsAffected()
+	if err != nil {
+		fmt.Print(err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	// Вырожденный способ собрать строку и отправить ее в качестве ответа в браузер
+	_, _ = io.WriteString(w, strconv.FormatInt(n, 10))
+}
+
+func indexPageHandler(w http.ResponseWriter, r *http.Request) {
+	_, _ = io.WriteString(w, "It's working!")
 }
